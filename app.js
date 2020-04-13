abf0760c14609cb0aceaeb24d417490cc9c0eea2
Marian Silviu Talmacel <mariansilviutalmacel@Marians-MacBook-Pro.local>
2020-04-13 16:13:13 +0100

    whack a mole

---
+++ app.js
@@ -0,0 +1,52 @@
+const squares = document.querySelectorAll(".square");
+const mole = document.querySelectorAll(".mole");
+const timeLeft = document.querySelector("#time-left");
+let score = document.querySelector("#score");
+let finalScore = document.querySelector(".final-score");
+const finalMole = document.querySelector(".finalMole");
+
+let result = 0;
+let currentTime = timeLeft.textContent;
+score.textContent = result;
+
+const randomSquare = () => {
+  squares.forEach((className) => {
+    className.classList.remove("mole");
+  });
+  if (timeLeft.textContent === "0") {
+    hitPosition = null;
+    finalScore.textContent = `GAME OVER. Your final score is ${result}`;
+    finalMole.classList.add("mole");
+    return;
+  }
+  let randomPosition = squares[Math.floor(Math.random() * 9)];
+  randomPosition.classList.add("mole");
+  hitPosition = randomPosition.id;
+};
+
+squares.forEach((id) => {
+  id.addEventListener("mouseup", () => {
+    if (id.id === hitPosition) {
+      result++;
+      score.textContent = result;
+      randomSquare();
+    }
+  });
+});
+
+const moveMole = () => {
+  let timerId = null;
+  timerId = setInterval(randomSquare, 500);
+};
+
+moveMole();
+
+const countDown = () => {
+  currentTime--;
+  timeLeft.textContent = currentTime;
+  if (currentTime === 0) {
+    clearInterval(timerId);
+  }
+};
+
+let timerId = setInterval(countDown, 1000);
